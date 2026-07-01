e7f31a2036e8cfd650c1d8f98160668000446cf3
Mo Monfared <86447387+MowMonfared@users.noreply.github.com>
2026-07-01 18:59:31 -0400

    Added sidebar from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
-import { Card } from './components/ui/card';
-import './App.css';
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 function App() {
   return (
-    <>
-      <Card />
-    </>
+    <TooltipProvider>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarInset>
+          <header className="flex h-16 items-center border-b px-4">
+            <SidebarTrigger />
+            <h1 className="ml-4 text-xl font-semibold">Dashboard</h1>
+          </header>
+
+          <main className="p-6">
+            <h2 className="text-2xl font-bold">Hello</h2>
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
   );
 }
 
